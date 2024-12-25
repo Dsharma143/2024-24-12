debe9ac3a5036ef5c7a4925db78d5f8f7c07718f
Dsharma143 <deepaksharma143@hotmail.com>
2024-12-25 13:13:39 -0500

    Changing into z.py

--- z.py
+++ z.py
@@ -2,3 +2,27 @@ x = 10
 y = 20
 z= x+y
 print(z)
+total 21
+drwxr-xr-x 1 deepa 197610   0 Dec 24 16:24 ./
+drwxr-xr-x 1 deepa 197610   0 Dec 24 10:24 ../
+drwxr-xr-x 1 deepa 197610   0 Dec 25 13:11 .git/
+-rw-r--r-- 1 deepa 197610 441 Dec 24 10:28 a.txt
+-rw-r--r-- 1 deepa 197610  17 Dec 24 16:25 abc.py
+-rw-r--r-- 1 deepa 197610   0 Dec 24 10:26 b.txt
+-rw-r--r-- 1 deepa 197610   0 Dec 24 10:34 c.txt
+-rw-r--r-- 1 deepa 197610   0 Dec 24 10:42 d.txt
+-rw-r--r-- 1 deepa 197610 405 Dec 24 10:42 e.txt
+-rw-r--r-- 1 deepa 197610 477 Dec 24 11:55 f.txt
+-rw-r--r-- 1 deepa 197610  34 Dec 24 16:19 z.py
+total 21
+drwxr-xr-x 1 deepa 197610   0 Dec 24 16:24 ./
+drwxr-xr-x 1 deepa 197610   0 Dec 24 10:24 ../
+drwxr-xr-x 1 deepa 197610   0 Dec 25 13:11 .git/
+-rw-r--r-- 1 deepa 197610 441 Dec 24 10:28 a.txt
+-rw-r--r-- 1 deepa 197610  17 Dec 24 16:25 abc.py
+-rw-r--r-- 1 deepa 197610   0 Dec 24 10:26 b.txt
+-rw-r--r-- 1 deepa 197610   0 Dec 24 10:34 c.txt
+-rw-r--r-- 1 deepa 197610   0 Dec 24 10:42 d.txt
+-rw-r--r-- 1 deepa 197610 405 Dec 24 10:42 e.txt
+-rw-r--r-- 1 deepa 197610 477 Dec 24 11:55 f.txt
+-rw-r--r-- 1 deepa 197610 577 Dec 25 13:11 z.py
